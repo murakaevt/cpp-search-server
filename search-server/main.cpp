c81358fb98bd1c96af18d77bdffc7f6aa8d30607
Тимур Муракаев <murakaevt88@yandex.ru>
2022-12-11 19:36:47 +0500

    first commit to the project

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,3 +2,21 @@
 // Напишите ответ здесь:
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
+#include <iostream>
+#include <string>
+#include <algorithm> // Закомитьте изменения и отправьте их в свой репозиторий.
+using namespace std;
+int main() {
+	int three_count = 0;
+	for (int three = 0; three <= 1000; ++three) {
+		string s = to_string(three);
+		for (char i : s) {
+			if (i == '3') {
+				++three_count;
+				break;
+			}
+		}
+	}
+	cout << three_count;
+	return 0;
+}
